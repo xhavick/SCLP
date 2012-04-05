28cd07ad237d211d998a43ffb972ad00bf53d1be
Carlangueitor <chack14rock@hotmail.com>
2012-04-05 16:56:28 -0500

    Agregando Iconos a la Ventana principal

--- gui/Windows.py
+++ gui/Windows.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 04/04/2012
 
@@ -8,15 +9,31 @@ Clases de Ventanas de la intefaz gráfica.
 
 from PyQt4 import QtGui
 
-class MainWindow(QtGui.QWidget):
+class MainWindow(QtGui.QMainWindow):
     '''
     Ventana Principal de la aplicación.
     ''' 
     def __init__(self):
-        QtGui.QWidget.__init__()
+        super(MainWindow, self).__init__()
         self.initUi()
         
     def initUi(self):
-        pass
+        self.setStatusBar(StatusBar())
+        self.setGeometry(0,0,800,600)
+        self.center()
+        self.setWindowTitle("SCLP Sistema de Control Logistico Promorutas v0.01")
+        self.setWindowIcon(QtGui.QIcon('gui/icon.png'))
+        self.show()
         
+    def center(self):
+        frameGem = self.frameGeometry()
+        centre = QtGui.QDesktopWidget().availableGeometry().center()
+        frameGem.moveCenter(centre)
+        self.move(frameGem.topLeft()) 
         
+class StatusBar(QtGui.QStatusBar):
+    '''
+    Barra de Estado Principal
+    '''
+    def __init__(self):
+        super(StatusBar, self).__init__()       
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+# -*- coding: utf-8 -*-
+
 '''
 Created on 04/04/2012
 
 @author: Charly
 '''
+
+import ctypes
+import sys
+from PyQt4 import QtGui
 from gui import Windows
 
+def main():
+    app = QtGui.QApplication(sys.argv)
+    app.setStyle('Cleanlooks')
+    mainWin = Windows.MainWindow()
+    sys.exit(app.exec_())
+
 if __name__ == '__main__':
-    pass
+    appid = 'revolucionip.scl.sclp.01'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
+    main()
+else:
+    print "Inicie este Archivo directamente"
+    
